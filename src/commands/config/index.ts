8709b414c8f9302de944988d55a416d40931b3f8
AnishDe12020 <63192115+AnishDe12020@users.noreply.github.com>
2022-05-16 11:12:53 +0530

    feat: add a config command to set web3.storage api token

---
+++ src/commands/config/index.ts
@@ -0,0 +1,32 @@
+import { Command } from "@oclif/core";
+import chalk from "chalk";
+import Conf from "conf/dist/source";
+import inquirer from "inquirer";
+
+export default class Config extends Command {
+  static description = "Configure the CLI";
+
+  async run(): Promise<void> {
+    const userConf = new Conf({ projectSuffix: "cli" });
+
+    this.log(chalk.cyan("⚙️ Let us configure the CLI"));
+
+    inquirer
+      .prompt([
+        {
+          type: "password",
+          name: "web3StorageAPIToken",
+          message: "What is your Web3.storage API Token?",
+          default: userConf.get("web3StorageAPIToken"),
+          required: true,
+          mask: "*",
+        },
+      ])
+      .then(answers => {
+        userConf.set("web3StorageAPIToken", answers.web3StorageAPIToken);
+      })
+      .catch(error => {
+        this.error(chalk.red(error));
+      });
+  }
+}
